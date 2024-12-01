b649b3789c5c9bb1ce34044f189fca5c985dc54b
ksj321456 <ksj321456@gmail.com>
2024-12-01 13:52:55 +0900

    DrawingClient 중복 생성 수정

---
+++ src/Main.java
@@ -0,0 +1,35 @@
+import javax.swing.*;
+import java.awt.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+//        JFrame frame = new JFrame();
+//        frame.setVisible(true);
+//        frame.setTitle("test");
+//        frame.setLayout(new BorderLayout());
+//        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+//        frame.setSize(1000, 500);
+//
+//        frame.setLayout(new BorderLayout());
+//        frame.add(new DrawPanel(), BorderLayout.CENTER);
+//        frame.add(new DrawingSetting(), BorderLayout.NORTH);
+//        frame.add(new LeftUserPanel(), BorderLayout.WEST);
+//        frame.add(new RightUserPanel(), BorderLayout.EAST);
+
+//        JPanel panelCenter = new JPanel();
+//        JButton buttonCenter = new JButton("그림판");
+//        panelCenter.add(buttonCenter);
+//        frame.add(panelCenter, BorderLayout.CENTER);
+//
+//        JPanel panelDown = new JPanel();
+//        JButton buttonDown = new JButton("채팅창");
+//        panelDown.add(buttonDown);
+//        frame.add(panelDown, BorderLayout.SOUTH);
+//
+//        JPanel panelWest = new JPanel();
+//        JButton buttonWest = new JButton("채팅 로그");
+//        panelWest.add(buttonWest);
+//        frame.add(panelWest, BorderLayout.EAST);
+    }
+}
